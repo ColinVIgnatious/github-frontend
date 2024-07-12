c4b9fa420f121076e642d908d7aa1ec96e08bd44
ColinVIgnatious <colinv.ignatious@gmail.com>
2024-07-12 08:46:52 +0530

    second commit

--- src/components/UserSearch.js
+++ src/components/UserSearch.js
@@ -7,7 +7,7 @@ const UserSearch = ({ onSearch }) => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('/api/users', { username });
+      const response = await axios.get(`https://api.github.com/users/${username}`);
       onSearch(response.data);
     } catch (error) {
       console.error('Error fetching user:', error);
